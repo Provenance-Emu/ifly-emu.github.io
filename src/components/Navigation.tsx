c0fa2422c52ad05bd26ac88f35e6eae928bf6f5d
Joseph Mattiello <git@joemattiello.com>
2025-09-30 14:14:26 -0400

    Design: narrow container (xl:1100px), center navbar with container, tighten overall layout

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -32,7 +32,7 @@ const Navigation = () => {
 
   return (
     <nav className="bg-gray-900 text-white shadow-lg sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="container">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <div className="flex items-center flex-shrink-0">
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
         sm: '640px',
         md: '768px',
         lg: '1024px',
-        xl: '1200px',
+        xl: '1100px',
         '2xl': '72rem',
       },
     },
